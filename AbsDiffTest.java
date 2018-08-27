741568703e007d97309b9e0c13201bd7f9c1a97e
nrayapat <42463882+nrayapat@users.noreply.github.com>
2018-08-27 08:58:57 +0530

    Add files via upload

---
+++ AbsDiffTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AbsDiffTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ AverageMarksTest.java
@@ -0,0 +1,14 @@
+package com.capgemini.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AverageMarksTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
