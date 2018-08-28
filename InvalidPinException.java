1976f4d1600fe28cf0eeef5d1581aa524363ced0
nrayapat <42463882+nrayapat@users.noreply.github.com>
2018-08-28 17:04:39 +0530

    Add files via upload

---
+++ InvalidPinException.java
@@ -0,0 +1,9 @@
+package com.capgemini.day5.exception;
+
+public class InvalidPinException extends Exception {
+
+	public InvalidPinException(String message) {
+		super(message);
+	}
+}
+
---
+++ LowBalanceException.java
@@ -0,0 +1,9 @@
+package com.capgemini.day5.exception;
+
+public class LowBalanceException extends Exception {
+
+	public LowBalanceException(String message) {
+		super(message);
+	}
+}
+
